b96bce4dfefe4c6f7ab502cd3e4d104596bd2719
Dahaminethshani <dahaminethshani@gmail.com>
2025-08-13 18:55:21 +0530

    update

--- test_app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("Streamlit Test App")
-st.write("🎉 If you can see this, Streamlit is working fine!")
-
-name = st.text_input("Enter your name")
-if name:
-    st.success(f"Hello, {name}!")
